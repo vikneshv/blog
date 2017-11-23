86c4161ee1cc812b940f53e3f6b50d26245987c6
viknesh <vickypondi69@gmail.com>
2017-11-23 18:29:07 +0530

    new11

--- new.ts
+++ new.ts
@@ -6,29 +6,25 @@ $(selector).on('click', function () {
 });
 
       
-  let promise =  fetch('https://jsonprovider.herokuapp.com/posts/?limit=50&sort=id+desc');   
-  promise.then(response => response.json())        
- .then(data => 
- {   
+let promise =  fetch('https://jsonprovider.herokuapp.com/posts/?limit=50&sort=id+desc');   
+promise.then(response => response.json())        
+.then(data => 
+{   
 $.each(data,function(index, obj){  
     var title = obj.title; 
     var blog_id = obj.id;             
 $("#blog_id").prepend('<img src="1600.png" width="60px" height="60px" />&nbsp;&nbsp;<b>Title:</b>&nbsp;' + title + 
-'<br>' + blog_id + ' </br>');                  
-           
-        });          
-     });
-
-
-
+'<br>' + blog_id + ' </br>');                             
+ });          
+ });
 
 $('#new_blog').click(function () {
     $('#main_body').load("add.html");
 });
 
 const returnfunction = () =>
- {
-    $('#main_body').load("index.html");
+{
+ $('#main_body').load("index.html");
 }
 const myfunction = () =>{
     var title =(<HTMLInputElement> document.getElementById("#title")).value;
